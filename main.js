7e5a515036ddb017b0700207968cf34206ea72a5
Kimberly MB <k.mueses@yahoo.com>
2022-10-10 20:03:45 -0400

    "Starts project steps 1-3"

---
+++ main.js
@@ -0,0 +1,8 @@
+let userName ='Kimberly';
+
+userName ? console.log('Hello, ${userName}') : console.log('Hello!');
+
+let userQuestion = 'Will I become a Jr Developer?';
+
+console.log('${userName} has asked - ${userQuestion}');
+
